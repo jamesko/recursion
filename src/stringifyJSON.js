16de0c67fafcb91128fd6cfe2a0b6a156e3878d1
James Kolonusz <jamesak@gmail.com>
2014-11-26 13:34:18 -0800

    Add cases in recursion.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,23 @@
 
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
-  // your code goes here
+
+	var type = typeof(obj);
+
+	switch(type){
+		case "undefined":{return undefined};
+		case "null":{return null};
+		case "boolean":{return obj ? "true" : "false"};
+		case "number":{return obj.toString()};
+		case "string":{return "\""+obj+"\""};
+		case "array":{
+			//undefined or function type elements are stored in array as null
+			
+		};
+		case "object":{
+			//undefined or function type property values are skipped
+
+
+		};
+	}
 };
