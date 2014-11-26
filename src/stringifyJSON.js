f09bae4ecfc35ce44e42add5878ed544ea9ce23d
James Kolonusz <jamesak@gmail.com>
2014-11-26 14:14:47 -0800

    Add code for case array, and fix stringify of null.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,21 +5,31 @@
 var stringifyJSON = function(obj) {
 
 	var type = typeof(obj);
+	var objString = "";	
+	var p = "\"";
 
 	switch(type){
 		case "undefined":{return undefined};
-		case "null":{return null};
+		case "null":{return "null"};
 		case "boolean":{return obj ? "true" : "false"};
 		case "number":{return obj.toString()};
-		case "string":{return "\""+obj+"\""};
+		case "string":{return p+obj+p};
 		case "array":{
 			//undefined or function type elements are stored in array as null
-			
+			obj.forEach(function(element){
+				eType = typeof(element);
+				if(eType!=undefined && eType!="function"){
+					objString += stringifyJSON(element);
+				}
+				else
+				{
+					objString += "null";
+				}
+			})
+			return "["+objString+"]";
 		};
 		case "object":{
 			//undefined or function type property values are skipped
-
-
 		};
 	}
 };
