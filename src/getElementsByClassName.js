c7e5ef0a94e56e375a6305a9521b9c818abe390e
James Kolonusz <jamesak@gmail.com>
2014-11-27 13:00:17 -0800

    Add object to array function, and fix code for multiple classes.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,19 +5,26 @@
 
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
+
 	var result = [];
+
+	//helper function to convert object to an array
+	function O2A(obj){
+		var arr=[];
+		for(var key in obj){if (key!="length") arr.push(obj[key]);}
+		return arr;
+	}
+
+	//recursion function
 	function getDown(element){
-
-		if(element.classList[0] === className) result.push(element);
-		//if(element.childNodes.length === 0) return;
-
-		for(var i=0;i<element.childNodes.length;i++){
-
+		if(element.classList != undefined){
+			if((O2A(element.classList)).indexOf(className) != -1) result.push(element);
 		}
-
-	};
+		for(var i=0;i<element.childNodes.length;i++){
+			getDown(element.childNodes[i]);
+		}
+	}
 
 	getDown(document.body);
-
 	return result;
 };
