f2583966eb1bc8a8b34dc885b0d744c679ee022e
James Kolonusz <jamesak@gmail.com>
2014-11-27 13:37:39 -0800

    Use classList contains methods to replace object to array helper.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -8,17 +8,12 @@ var getElementsByClassName = function(className){
 
 	var result = [];
 
-	//helper function to convert object to an array
-	function O2A(obj){
-		var arr=[];
-		for(var key in obj){if (key!="length") arr.push(obj[key]);}
-		return arr;
-	}
-
 	//recursion function
 	function getDown(element){
 		if(element.classList != undefined){
-			if((O2A(element.classList)).indexOf(className) != -1) result.push(element);
+			if(element.classList.contains(className)){
+				result.push(element);
+			}
 		}
 		for(var i=0;i<element.childNodes.length;i++){
 			getDown(element.childNodes[i]);
